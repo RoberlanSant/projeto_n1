2ad35399b7a5e4d9195272f09497ff37c30cfed3
RoberlanSant <roberfalc@gmail.com>
2020-06-29 20:11:54 -0300

    atualizacao

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,84 +1,65 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
-
-
-hoteis = [
-        {
-            'hotel_id': 'alpha',
-            'nome': 'Alpha Hotel',
-            'estrelas': '4.3',
-            'diaria': '420.30',
-            'cidade': 'Rio de Janerio'
-        },
-        {
-            'hotel_id': 'bravo',
-            'nome': 'Bravo Hotel',
-            'estrelas': '3.3',
-            'diaria': '420.30',
-            'cidade': 'Santa Catarina'
-        },
-        {
-            'hotel_id': 'delta',
-            'nome': 'Delta Hotel',
-            'estrelas': '2.3',
-            'diaria': '420.30',
-            'cidade': 'São Paulo'
-        }
-]
-
-
+from flask_jwt_extended import jwt_required
 
 
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': hoteis}
+        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
 
 
 class Hotel(Resource):
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
+    argumentos.add_argument('nome', type=str, required=True, help="O campo 'nome' não pode ser deixado em branco.")
+    argumentos.add_argument('estrelas', type=float, required=True, help="O campo 'estrelas' não pode ser deixado em branco.")
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
 
 
     def get(self, hotel_id):
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return {'message': 'Hotel não encontrado.'}, 404
 
-
+    @jwt_required
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": "Hotel id '{}' ja existe.".format(hotel_id)}, 400
 
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()    
-        hoteis.append(novo_hotel)
-        return novo_hotel, 200
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'Erro interno ao tentar salva hotel.'}, 500
+        return hotel.json()
 
-
+    @jwt_required
     def put(self, hotel_id):
-
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json() 
-        hotel = Hotel.find_hotel(hotel_id)
-        if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 200
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201
+
+        hotel_localizado = HotelModel.find_hotel(hotel_id)
+        if hotel_localizado:
+            hotel_localizado.update_hotel(**dados)
+            hotel_localizado.save_hotel()
+            return hotel_localizado.json(), 200
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'Erro interno ao tentar salva hotel.'}, 500
+        return hotel.json(), 201
         
 
-
+    @jwt_required
     def delete(self, hotel_id):
-        global hoteis
-        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-        return {'message': 'Hotel deletado.'}
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try:
+                hotel.delete_hotel()
+            except:
+                return {'message': 'Erro interno ao tentar deletar hotel.'}, 500
+            return {'message': 'Hotel deletado.'}
+        return {'message': 'Hotel não existe.'}
